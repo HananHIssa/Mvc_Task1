67c8c8cd44f69e13fda7bce4ce4c9eb56381612d
HananHIssa <hananissa2018@gmail.com>
2024-09-13 00:17:17 +0300

    mvcTask

---
+++ WebApplication1/Controllers/StudentsController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController1 : Controller
+    {
+        public IActionResult getall()
+        {
+            return View();
+        }
+        public IActionResult create()
+        {
+            return View();
+        }
+        public IActionResult update()
+        {
+            return View();
+        }
+        public IActionResult delete()
+        {
+            return View();
+        }
+    }
+}
